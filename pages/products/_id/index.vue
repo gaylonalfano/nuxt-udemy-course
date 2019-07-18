692dd8de7d227870cadeabe0224e61ad340230e4
gaylonalfano <gaylon.alfano@gmail.com>
2019-07-18 10:56:21 -0500

    Initial commit. Started Nuxt course and have been learning how to do pages and routes using a folder-based approach. Just learned how to validate a user input by using the validate(data) method where you can access the parameters of the object that the user inputs by data.params.id for example. You can then do validation on this input. Even can use regex /^\d+$/.test(data.params.id) that will return true if it validates. Then and only then will the URL load since the _id is a dynamic segment. Read my notes. Next we are looking at creating nested routes where the content of one page can be loaded within another page.

---
+++ pages/products/_id/index.vue
@@ -0,0 +1,6 @@
+<template>
+  <h1>
+    This is the /products/_id page.
+    The ID entered is: {{ $route.params.id }}
+  </h1>
+</template>
---
+++ pages/users/_id/more-details.vue
@@ -0,0 +1,6 @@
+<template>
+  <h1>
+    More details view nested inside _id folder.
+    Here's the id from $route.params.id: {{ $route.params.id }}
+  </h1>
+</template>
